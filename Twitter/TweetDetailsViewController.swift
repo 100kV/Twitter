66e93be59d0d65dad055e28c6b9dd054454a00db
Kevin Raymundo <kraymundo@tumblr.com>
2015-09-14 22:50:41 -0700

    Details retweet and fav

--- Twitter/TweetDetailsViewController.swift
+++ Twitter/TweetDetailsViewController.swift
@@ -30,12 +30,87 @@ class TweetDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if (tweet.retweetedStatus == nil) {
+            retweetLabelButton.hidden = true
+            retweetUserScreenname.hidden = true
+        } else {
+            retweetLabelButton.hidden = false
+            retweetUserScreenname.hidden = false
+            retweetUserScreenname.text = "\(tweet.retweetUser!.screenname!) retweeted"
+        }
+        
+        var url = tweet.user?.profileImageUrl
+        if url != nil {
+            profileImageView.setImageWithURLRequest(NSURLRequest(URL: NSURL(string: url!)!), placeholderImage: nil, success: { (NSURLRequest, NSHTTPURLResponse, UIImage) -> Void in
+                self.profileImageView.image = UIImage
+                }) { (NSURLRequest, NSHTTPURLResponse, NSError) -> Void in
+                    println("Error setting tweet user profile image")
+            }
+        }
+        
+        nameLabel.text = tweet.user?.name
+        screennameLabel.text = "@\(tweet.user!.screenname!)"
+        tweetTextLabel.text = tweet.text
+        
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "MM/dd/YYYY hh:mm:ss a"
+        timestampLabel.text = formatter.stringFromDate(tweet.createdAt!)
+        
+        retweetCountLabel.text = "\(tweet.retweetCount!)"
+        favoriteCountLabel.text = "\(tweet.favoriteCount!)"
+        
+        if (tweet.retweeted == true) {
+            retweetButton.setImage(UIImage(named: "retweet_on.png"), forState: UIControlState.Normal)
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet.png"), forState: UIControlState.Normal)
+        }
+        
+        if (tweet.favorited == true) {
+            favoritedButton.setImage(UIImage(named: "favorite_on.png"), forState: UIControlState.Normal)
+        } else {
+            favoritedButton.setImage(UIImage(named: "favorite.png"), forState: UIControlState.Normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClickRetweetButton(sender: AnyObject) {
+        var params = ["id": tweet.id!]
+        
+        if (tweet.retweeted == true) {
+            if (tweet.retweetId != nil) {
+                params["id"] = tweet.retweetId!
+                TwitterClient.sharedInstance.statusesDestroy(params, completion: { (tweet, error) -> () in
+                    self.retweetButton.setImage(UIImage(named: "retweet.png"), forState: UIControlState.Normal)
+                    self.tweet.retweeted = false
+                })
+            }
+        } else {
+            TwitterClient.sharedInstance.statusesRetweets(params, completion: { (tweet, error) -> () in
+                self.retweetButton.setImage(UIImage(named: "retweet_on.png"), forState: UIControlState.Normal)
+                self.tweet.retweeted = true
+                self.tweet.retweetId = tweet?.id
+            })
+        }
+    }
+    
+    @IBAction func onClickFavoritedButton(sender: AnyObject) {
+        var params = ["id": tweet.id!]
+        
+        if (tweet.favorited == true) {
+            TwitterClient.sharedInstance.favoritesDestroy(params, completion: { (tweet, error) -> () in
+                self.favoritedButton.setImage(UIImage(named: "favorite.png"), forState: UIControlState.Normal)
+                self.tweet.favorited = false
+            })
+        } else {
+            TwitterClient.sharedInstance.favoritesCreate(params, completion: { (tweet, error) -> () in
+                self.favoritedButton.setImage(UIImage(named: "favorite_on.png"), forState: UIControlState.Normal)
+                self.tweet.favorited = true
+            })
+        }
     }
     
 
